4b1c500c16549c7add56d1a61f67b2c1f532e71a
Ghaiath <gm.trabulsi@gmail.com>
2018-02-03 19:18:00 +0100

    Creating a math operations class

--- src/math__operation.java
+++ src/math__operation.java
@@ -1,4 +1,22 @@
 
 public class math__operation {
+	
+    double add(double num1, double num2) {
+        double answer = num1+num2;
+        return answer;  }
+ 
+    double subtract(double num1, double num2) {
+        double answer = num1-num2;
+        return answer;          
+    }
 
+    double multiply(double num1, double num2) {
+        double answer = num1*num2;
+        return answer;          
+    }
+
+    double divide(double num1, double num2) {
+        double answer = num1/num2;
+        return answer;          
+    }
 }
