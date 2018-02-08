4499455f86cd755381480074897ed002d9aa7f60
Ghaiath <gm.trabulsi@gmail.com>
2018-02-08 12:16:06 +0100

    Lägga till avrunding för resultatet

--- src/math__operation.java
+++ src/math__operation.java
@@ -3,20 +3,20 @@ public class math__operation {
 	
     double add(double num1, double num2) {
         double answer = num1+num2;
-        return answer;  }
+        return Math.round(answer*1000d)/1000d;  }
  
     double subtract(double num1, double num2) {
         double answer = num1-num2;
-        return answer;          
+        return Math.round(answer*1000d)/1000d;          
     }
 
     double multiply(double num1, double num2) {
         double answer = num1*num2;
-        return answer;          
+        return Math.round(answer*1000d)/1000d;          
     }
 
     double divide(double num1, double num2) {
         double answer = num1/num2;
-        return answer;    
+        return Math.round(answer*1000d)/1000d;    
     }
 }
